f3c259f09931bf5468d5a10aec18be1d5f0cc3de
Dawid Ciok <dawid.ciok@gmail.com>
2017-03-31 17:47:46 +0200

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def hello
+  	render html: "Ekki Vera Hrædd, Þú Ert Bara Með Augun Lokuð"
+  end
 end
